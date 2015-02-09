0ec88879d8e08b3758c97a8986479ea9d2250fd1
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-02-09 15:16:55 +0300

    Add utils tests

--- src/alley_services_utils.erl
+++ src/alley_services_utils.erl
@@ -5,6 +5,12 @@
     calc_parts_number/2
 ]).
 
+%-define(TEST, 1).
+-ifdef(TEST).
+    -compile(export_all).
+    -include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -include_lib("alley_dto/include/adto.hrl").
 
 -define(TON_UNKNOWN,       0).
@@ -65,3 +71,41 @@ calc_parts_number(Size, ucs2) ->
         true ->  trunc(Size/67);
         false -> trunc(Size/67) + 1
     end.
+
+%% ===================================================================
+%% Begin Tests
+%% ===================================================================
+
+-ifdef(TEST).
+
+addr_to_dto_test() ->
+    Addr_11 = <<"375296543210">>,
+    Addr1_60 = <<"1">>,
+    Addr2_60 = <<"123456">>,
+    Addr_50 = <<"anything">>,
+    ?assertEqual(#addr{addr= <<"375296543210">>,ton=1,npi=1}, addr_to_dto(Addr_11)),
+    ?assertEqual(#addr{addr= <<"1">>,ton=6,npi=0}, addr_to_dto(Addr1_60)),
+    ?assertEqual(#addr{addr= <<"123456">>,ton=6,npi=0}, addr_to_dto(Addr2_60)),
+    ?assertEqual(#addr{addr= <<"anything">>,ton=5,npi=0}, addr_to_dto(Addr_50)).
+
+calc_parts_number_default_test() ->
+    ?assertEqual(1, calc_parts_number(160, default)),
+    ?assertEqual(2, calc_parts_number(161, default)),
+    ?assertEqual(2, calc_parts_number(306, default)),
+    ?assertEqual(3, calc_parts_number(307, default)),
+    ?assertEqual(3, calc_parts_number(459, default)),
+    ?assertEqual(4, calc_parts_number(460, default)).
+
+calc_parts_number_ucs2_test() ->
+    ?assertEqual(1, calc_parts_number(70, ucs2)),
+    ?assertEqual(2, calc_parts_number(71, ucs2)),
+    ?assertEqual(2, calc_parts_number(134, ucs2)),
+    ?assertEqual(3, calc_parts_number(135, ucs2)),
+    ?assertEqual(3, calc_parts_number(201, ucs2)),
+    ?assertEqual(4, calc_parts_number(202, ucs2)).
+
+-endif.
+
+%% ===================================================================
+%% End Tests
+%% ===================================================================
