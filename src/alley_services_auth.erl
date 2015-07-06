1ff08f6949d1ce877b6ea6e7c0feccb6fb701ce4
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-07-06 13:46:06 +0300

    Add auth by msisdn and generalize

--- src/alley_services_auth.erl
+++ src/alley_services_auth.erl
@@ -6,7 +6,8 @@
 -export([
     start_link/0,
     authenticate/4,
-    authenticate_by_email/2
+    authenticate_by_email/2,
+    authenticate_by_msisdn/2
 ]).
 
 -include("application.hrl").
@@ -22,16 +23,21 @@ start_link() ->
     {ok, QueueName} = application:get_env(?APP, kelly_auth_queue),
     rmql_rpc_client:start_link(?MODULE, QueueName).
 
--spec authenticate(binary(), binary(), atom(), binary()) ->
-    {ok, #auth_resp_v1{}} | {error, timeout}.
-authenticate(CustomerId, UserId, Interface, Password) ->
-    case request_backend(CustomerId, UserId, Interface, Password) of
-        {ok, AuthResp = #auth_resp_v1{result = Result}} ->
+-spec authenticate(binary(), binary(), binary(), atom()) ->
+    {ok, #auth_resp_v2{}} | {error, timeout}.
+authenticate(CustomerId, UserId, Password, Interface) ->
+    AuthData = #auth_credentials{
+        customer_id = CustomerId,
+        user_id = UserId,
+        password = Password
+    },
+    case request_backend(AuthData, Interface) of
+        {ok, AuthResp = #auth_resp_v2{result = Result}} ->
             case Result of
                 #auth_customer_v1{} ->
                     ok = alley_services_auth_cache:store(
                         CustomerId, UserId, Interface, Password, AuthResp);
-                #auth_error_v1{} ->
+                #auth_error_v2{} ->
                     ok
             end,
             {ok, AuthResp};
@@ -48,66 +54,41 @@ authenticate(CustomerId, UserId, Interface, Password) ->
             end
     end.
 
--spec authenticate_by_email(binary(), binary()) ->
+-spec authenticate_by_email(binary(), atom()) ->
     {ok, #auth_resp_v2{}} | {error, timeout}.
 authenticate_by_email(Email, Interface) ->
-    case request_backend(Email, Interface) of
-        {ok, AuthResp = #auth_resp_v2{result = Result}} ->
-            case Result of
-                #auth_customer_v1{} ->
-                    ok = alley_services_auth_cache:store_by_email(
-                        Email, Interface, AuthResp);
-                #auth_error_v2{} ->
-                    ok
-            end,
+    AuthData = #auth_email{
+        email = Email
+    },
+    case request_backend(AuthData, Interface) of
+        {ok, AuthResp = #auth_resp_v2{}} ->
             {ok, AuthResp};
         {error, timeout} ->
-            ?log_debug("Trying auth cache...", []),
-            case alley_services_auth_cache:fetch_by_email(Email, Interface) of
-                {ok, AuthResp} ->
-                    ?log_debug("Found auth response: ~p", [AuthResp]),
-                    {ok, AuthResp};
-                not_found ->
-                    ?log_error("Not found auth response.", []),
-                    {error, timeout}
-            end
+            {error, timeout}
+    end.
+
+-spec authenticate_by_msisdn(#addr{}, atom()) ->
+    {ok, #auth_resp_v2{}} | {error, timeout}.
+authenticate_by_msisdn(Msisdn, Interface) ->
+    AuthData = #auth_msisdn{
+        msisdn = Msisdn
+    },
+    case request_backend(AuthData, Interface) of
+        {ok, AuthResp = #auth_resp_v2{}} ->
+            {ok, AuthResp};
+        {error, timeout} ->
+            {error, timeout}
     end.
 
 %% ===================================================================
 %% Internal
 %% ===================================================================
 
-request_backend(CustomerId, UserId, Interface, Password) ->
-    ReqId = uuid:unparse(uuid:generate_time()),
-    AuthReq = #auth_req_v1{
-        req_id = ReqId,
-        customer_id = CustomerId,
-        user_id = UserId,
-        password = Password,
-        interface = Interface
-    },
-    ?log_debug("Sending auth request: ~p", [AuthReq]),
-    {ok, Payload} = adto:encode(AuthReq),
-    case rmql_rpc_client:call(?MODULE, Payload, <<"AuthReqV1">>) of
-        {ok, Bin} ->
-            case adto:decode(#auth_resp_v1{}, Bin) of
-                {ok, AuthResp} ->
-                    ?log_debug("Got auth response: ~p", [AuthResp]),
-                    {ok, AuthResp};
-                {error, Error} ->
-                    ?log_error("Auth response decode error: ~p", [Error]),
-                    {error, Error}
-            end;
-        {error, timeout} ->
-            ?log_error("Got auth response: timeout", []),
-            {error, timeout}
-    end.
-
-request_backend(Email, Interface) ->
+request_backend(AuthData, Interface) ->
     ReqId = uuid:unparse(uuid:generate_time()),
     AuthReq = #auth_req_v2{
         req_id = ReqId,
-        email = Email,
+        auth_data = AuthData,
         interface = Interface
     },
     ?log_debug("Sending auth request: ~p", [AuthReq]),
