19d6a47dc7ff85e3ef9e594cf88791d312679813
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-08-27 18:36:54 +0300

    Refine check logic

--- src/alley_services_blacklist.erl
+++ src/alley_services_blacklist.erl
@@ -31,6 +31,12 @@
 -include_lib("alley_common/include/logging.hrl").
 -include_lib("alley_common/include/gen_server_spec.hrl").
 
+%-define(TEST, 1).
+-ifdef(TEST).
+    -compile(export_all).
+    -include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -record(st, {
     timer_ref :: reference()
 }).
@@ -145,14 +151,17 @@ check(DstAddr, SrcAddr, Tab) ->
     case ets:lookup(Tab, DstAddr) of
         [] ->
             allowed;
-        [{DstAddr, undefined}] ->
-            denied;
         Entries ->
-            case lists:member({DstAddr, SrcAddr}, Entries) of
+            case lists:member({DstAddr, undefined}, Entries) of
                 true ->
                     denied;
                 false ->
-                    allowed
+                    case lists:member({DstAddr, SrcAddr}, Entries) of
+                        true ->
+                            denied;
+                        false ->
+                            allowed
+                    end
             end
     end.
 
@@ -165,3 +174,101 @@ filter([DstAddr | DstAddrs], SrcAddr, Allowed, Denied) ->
     end;
 filter([], _SrcAddr, Allowed, Denied) ->
     {Allowed, Denied}.
+
+%% ===================================================================
+%% Begin Tests
+%% ===================================================================
+
+-ifdef(TEST).
+
+get_blacklist() ->
+    {ok, #blacklist_resp_v1{
+        req_id = <<"b8312504-4cc8-11e5-9139-ae3a4af2dd4b">>,
+        entries = [
+            %% entry 1
+            #blacklist_entry_v1{
+                id = <<"0118e9da-be64-11e4-bdd1-ae3a4af2dd4b">>,
+                dst_addr = #addr{addr = <<"375295555555">>, ton = 1, npi = 1},
+                src_addr = #addr{addr = <<"491111111111">>, ton = 1, npi = 1}
+            },
+            %% entry 2
+            #blacklist_entry_v1{
+                id = <<"24a8a0a2-4ca8-11e5-ae57-ae3a4af2dd4b">>,
+                dst_addr = #addr{addr = <<"375296666666">>, ton = 1, npi = 1},
+                src_addr = #addr{addr = <<"or125">>, ton = 5, npi = 0}
+            },
+            %% entry 3
+            #blacklist_entry_v1{
+                id = <<"71d02610-4cc2-11e5-bf89-ae3a4af2dd4b">>,
+                dst_addr = #addr{addr = <<"375296666666">>, ton = 1, npi = 1},
+                src_addr = undefined
+            }
+        ]}}.
+
+blacklist_test_() ->
+    {setup,
+        fun test_start/0,
+        fun test_stop/1,
+        fun(Pid) ->
+            [
+                test_update(Pid),
+                test_check(Pid),
+                test_filter(Pid)
+            ]
+        end
+    }.
+
+test_start() ->
+    ok = application:start(meck),
+    ok = meck:new(alley_services_api, [non_strict]),
+    ok = meck:expect(alley_services_api, get_blacklist, fun get_blacklist/0),
+    {ok, Pid} = alley_services_blacklist:start_link(),
+    Pid.
+
+test_stop(Pid) ->
+    ok = application:stop(meck),
+    exit(Pid, normal).
+
+test_update(_Pid) ->
+    Res = alley_services_blacklist:update(),
+    [?_assertEqual(ok, Res)].
+
+test_check(_Pid) ->
+    Orig1 = #addr{addr = <<"491111111111">>, ton = 1, npi = 1},
+    Orig2 = #addr{addr = <<"or125">>, ton = 5, npi = 0},
+
+    Dest1 = #addr{addr = <<"375295555555">>, ton = 1, npi = 1},
+    Dest2 = #addr{addr = <<"375296666666">>, ton = 1, npi = 1},
+    Dest3 = #addr{addr = <<"375297777777">>, ton = 1, npi = 1},
+
+    [
+    ?_assertEqual(denied, alley_services_blacklist:check(Dest1, Orig1)),  % entry 1
+    ?_assertEqual(allowed, alley_services_blacklist:check(Dest1, Orig2)),
+
+    ?_assertEqual(denied, alley_services_blacklist:check(Dest2, Orig1)),  % entry 3
+    ?_assertEqual(denied, alley_services_blacklist:check(Dest2, Orig2)),  % entry 2
+
+    ?_assertEqual(allowed, alley_services_blacklist:check(Dest3, Orig1)),
+    ?_assertEqual(allowed, alley_services_blacklist:check(Dest3, Orig2))
+    ].
+
+test_filter(_Pid) ->
+    Orig1 = #addr{addr = <<"491111111111">>, ton = 1, npi = 1},
+    Orig2 = #addr{addr = <<"or125">>, ton = 5, npi = 0},
+
+    Dest1 = #addr{addr = <<"375295555555">>, ton = 1, npi = 1},
+    Dest2 = #addr{addr = <<"375296666666">>, ton = 1, npi = 1},
+    Dest3 = #addr{addr = <<"375297777777">>, ton = 1, npi = 1},
+
+    Dests = [Dest1, Dest2, Dest3],
+
+    [
+    ?_assertEqual({[Dest3], [Dest2, Dest1]}, alley_services_blacklist:filter(Dests, Orig1)),
+    ?_assertEqual({[Dest3, Dest1], [Dest2]}, alley_services_blacklist:filter(Dests, Orig2))
+    ].
+
+-endif.
+
+%% ===================================================================
+%% End Tests
+%% ===================================================================
